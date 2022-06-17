20311943b615aa9411b1a921514b0964c27659d7
mailakash18 <107082241+mailakash18@users.noreply.github.com>
2022-06-17 15:02:08 +0200

    Update_first_ec2

--- First_ec2.tf
+++ First_ec2.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-
-  region ="us-east-1"
-  access_key = "AKIA5XO35XJP6DE4KAWP"
-  secret_key = "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"
-}
-
 resource "aws_instance" "myec2" {
 
   ami = "ami-0022f774911c1d690"
